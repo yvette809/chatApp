ce47ebd7a4d27e9db1779932cbbe07db8647d8ff
Yvette <nchombuayvta@gmil.com>
2020-09-09 18:53:20 +0200

    mesages added

--- chat_app server/server.js
+++ chat_app server/server.js
@@ -4,30 +4,48 @@ const http = require("http");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {addUser, removeUser,getUser, getUsersInRoom} = require("./src/users")
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./src/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket)=>{
-    console.log("we have a new connection!!")
+io.on("connection", (socket) => {
+  console.log("we have a new connection!!");
 
-    // all codes will run here
-    socket.on('join', ({name,room})=>{
-        console.log(name,room)
-    })
+  // all codes will run here
+  // 1. Join room
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room }); //addUser() can return either an error or a user. We get the id from the ocket id
+    if (error) {
+      return callback(error);
+    } else {
+      // join a user to a room
+      socket.join(user.room);
+      socket.emit('message', {user: 'admin', text: `${user.name}welcome to the room ${user.room}`})
+      //the message below is telling evryone that the user has joined
+      socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined}`})
+      callback()
+    }
+  });
 
-    
-    socket.on('disconnect', ()=>{
-        console.log('user has left!!')
-    })
-})
+  //2.send message
+
+  socket.on('sendMessage', (message,callback)=>{
+      const user = getUser(socket.id)
+      io.to(user.room).emit('message', {user: user.name, text:message});
+      callback()
+  })
+
+  socket.on("disconnect", () => {
+    console.log("user has left!!");
+  });
+});
 
 const port = process.env.PORT || 3006;
 
-const router = require("./src/router")
-app.use("/", router)
+const router = require("./src/router");
+app.use("/", router);
 
 server.listen(port, () => {
   console.log(`server is running on port ${port}`);
--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,38 +1,69 @@
-import React,{useState, useEffect} from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
+import InforBar from "../InfoBar/InfoBar.jsx";
+import Input from "../Input/Input.jsx";
+import Messages from "../Messages/Messages";
 
 let socket;
 
-const Chat = ({location}) => {
-
+const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const ENDPOINT = 'localhost:3005'
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "localhost:3005";
 
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io();
+    setName(name);
+    setRoom(room);
+    // console.log(socket)
+    socket.emit("join", { name, room }, () => {});
 
-    useEffect(()=>{
-        const {name,room} = queryString.parse(location.search)
-        socket = io()
-        setName(name);
-        setRoom(room);
-        // console.log(socket)
-        socket.emit('join', {name,room}, ()=>{
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
 
-        });
-        
-        return()=>{
-            socket.emit('disconnect')
-            socket.off()
-        }
-    },[ENDPOINT,location.search])
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
 
-    return (
-        <div>
-            chat
-        </div>
-    )
-}
+  // function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  };
+  console.log(message, messages);
 
-export default Chat
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InforBar room={room} />
+        <Messages messages={messages}/>
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+        {/* <input
+          value={message}
+          onChange={(e) => setMessages(e.currentTarget.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage() : null)}
+        /> */}
+      </div>
+    </div>
+  );
+};
 
+export default Chat;
